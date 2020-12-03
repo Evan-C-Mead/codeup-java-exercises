3af3802ca6de23c6f2a2556564e1bf75a7c10935
Evan Mead <evan.c.mead@gmail.com>
2020-12-03 14:57:30 -0600

    exercise three complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,16 +4,27 @@ import static java.lang.Integer.parseInt;
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        for (double i = 1; i <= 100; i++){
-            if (i % 3 == 0){
-                System.out.printf("Fizz %.0f\n", i);
-            } else if (i % 5 == 0){
-                System.out.printf("Buzz %.0f\n", i);
-            } else if (i % 3 == 0 && i % 5 == 0){
-                System.out.printf("FizzBuzz %.0f\n", i);
-            } else {
-                System.out.printf("%.0f\n", i);
+        Scanner userInput = new Scanner(System.in);
+        userInput.useDelimiter("\n");
+
+
+        System.out.println("Enter a number. You will see a table of squared and cubed numbers up to the number you have entered.");
+        double input = userInput.nextDouble();
+
+        System.out.println("Would you like to continue? [Y/N]");
+        String userConfirmation = userInput.next();
+        boolean confirm = userConfirmation.equalsIgnoreCase("Y");
+
+        if (confirm) {
+            System.out.println("Number | Squared | Cubed");
+            System.out.println("------ | ------- | -----");
+            for (double i = 1; i <= input; i++ ){
+                double squared = Math.pow(i, 2);
+                double cubed = Math.pow(i, 3);
+                System.out.printf("%.0f      | %.0f       | %.0f\n", i, squared, cubed);
             }
+        } else {
+            System.out.println("Program terminated");
         }
 
     }
