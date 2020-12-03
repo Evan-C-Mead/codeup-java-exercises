76282fceec49b0f1be6560414c710f6c9f1f8fd8
Evan Mead <evan.c.mead@gmail.com>
2020-12-03 13:15:39 -0600

    exercise two complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,36 +4,17 @@ import static java.lang.Integer.parseInt;
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        int i = 5;
-        while (i <= 15){
-            System.out.println(i);
-            i++;
+        for (double i = 1; i <= 100; i++){
+            if (i % 3 == 0){
+                System.out.printf("Fizz %.0f\n", i);
+            } else if (i % 5 == 0){
+                System.out.printf("Buzz %.0f\n", i);
+            } else if (i % 3 == 0 && i % 5 == 0){
+                System.out.printf("FizzBuzz %.0f\n", i);
+            } else {
+                System.out.printf("%.0f\n", i);
+            }
         }
 
-        int x = 0;
-        do {
-            System.out.println(x);
-            x += 2;
-        } while (x <= 100);
-
-        int y = 100;
-        do {
-            System.out.println(y);
-            y -= 5;
-        } while (y >= -10);
-
-        double z = 2;
-        do {
-            System.out.printf("%.0f\n", z);
-            z = Math.pow(z, 2);
-        } while (z < 1000000);
-
-        for (int yy = 100; yy >= -10; yy -= 5){
-            System.out.println(yy);
-        }
-
-        for (double zz = 2; zz < 1000000; zz = Math.pow(zz, 2)){
-            System.out.printf("%.0f\n", zz);
-        }
     }
 }
