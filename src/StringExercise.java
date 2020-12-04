bf9fe77652bc65637d152535cfc33cc3207fe3ab
Evan Mead <evan.c.mead@gmail.com>
2020-12-04 13:31:11 -0600

    letter counter created

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -14,5 +14,33 @@ public class StringExercise {
 
         String stringFour = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
         System.out.println(stringFour);
+
+//        do {
+//            System.out.println("Type something and I'll count the consonants and vowels!");
+//            String input = sc.nextLine();
+//            int vowels = 0, consonants = 0;
+//            input = input.toLowerCase();
+//
+//            for (int i = 0; i < input.length(); ++i)
+//            {
+//                char letter = input.charAt(i);
+//                if (letter == 'a' || letter == 'e' || letter == 'i'
+//                        || letter == 'o' || letter == 'u') {
+//                    ++vowels;
+//                }
+//                else if ((letter >= 'a'&& letter <= 'z')) {
+//                    ++consonants;
+//                }
+//            }
+//            System.out.println("Number of consonants: " + consonants);
+//            System.out.println("Number of vowels: " + vowels);
+//            System.out.println("Try again? [y/n]");
+//            String typeAgain = sc.nextLine();
+//            if (typeAgain.equals("n")) {
+//                System.out.println("Program terminated");
+//                break;
+//            }
+//        }while (true);
+
     }
 }
