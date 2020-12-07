9e1896d4176780e0cd4b994102768a864aae225d
Evan Mead <evan.c.mead@gmail.com>
2020-12-07 09:26:02 -0600

    exercise three complete

--- src/MethodExercises.java
+++ src/MethodExercises.java
@@ -14,25 +14,33 @@ public class MethodExercises {
         // Exercise #2
         getInteger(1, 10);
 
+        // Exercise #3
+        factorial();
+
     }
 
     public static double addition(double x, double y) {
+
         return x + y;
     }
 
     public static double subtraction(double x, double y) {
+
         return x - y;
     }
 
     public static double multiplication(double x, double y) {
+
         return x * y;
     }
 
     public static double division(double x, double y) {
+
         return x / y;
     }
 
     public static double modulus(double x, double y) {
+
         return x % y;
     }
 
@@ -43,6 +51,25 @@ public class MethodExercises {
             int userInput = sc.nextInt();
             if (userInput > min & userInput < max) {
                 System.out.printf("Your number was: %d\n", userInput);
+                break;
+            }
+        } while (true);
+    }
+
+    public static void factorial() {
+        do {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a number between 1 and 10: ");
+            int userNumber = sc.nextInt();
+            int sum = 1;
+            for (int i = 1; i <= userNumber; i++) {
+                sum *= i;
+            }
+            System.out.printf("The factorial of %d is: %d \n", userNumber, sum);
+            System.out.println("Do you want to continue? [y/n] ");
+            String option = sc.next();
+            if (!option.equalsIgnoreCase("y")) {
+                System.out.println("Project terminated");
                 break;
             }
         } while (true);
