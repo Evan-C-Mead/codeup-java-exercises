fc3f4e5f22a1ec87ecd1741f2c5faa6f6bff93bb
Evan Mead <evan.c.mead@gmail.com>
2020-12-07 10:56:44 -0600

    high low guessing game created

---
+++ src/HighLow.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class HighLow {
+    public static void main(String[] args) {
+        guess();
+    }
+
+    public static void guess() {
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(100) + 1;
+        int guessCount = 0;
+
+        System.out.println("Guess a number between 1 and 100...");
+        do {
+            Scanner sc = new Scanner(System.in);
+            int userGuess = sc.nextInt();
+            if (userGuess < randomNumber) {
+                System.out.println("HIGHER");
+                guessCount++;
+            } else if (userGuess > randomNumber) {
+                System.out.println("LOWER");
+                guessCount++;
+            } else if (userGuess == randomNumber) {
+                System.out.println("GOOD GUESS!");
+                System.out.printf("It took you %d guesses.", guessCount);
+                break;
+            }
+        } while (true);
+    }
+
+}
