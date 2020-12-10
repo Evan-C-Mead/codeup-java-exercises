cb3cab0dca459246fe32c2a80eea10527a3b11c0
Evan Mead <evan.c.mead@gmail.com>
2020-12-10 11:43:39 -0600

    circle bonus

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -5,16 +5,23 @@ public class Circle {
     private double radius;
     double pi = Math.PI;
 
+    private static int circleCount = 0;
+
     public Circle(double radius) {
         this.radius = radius;
+        circleCount++;
     }
 
     public double getArea() {
-        return pi * this.radius * this.radius;
+        return pi * (Math.pow(radius, 2));
     }
 
     public double getCircumference() {
         return 2 * pi * this.radius;
+    }
+
+    public static int getCircleCount() {
+        return circleCount;
     }
 
 }
--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -6,11 +6,25 @@ public class CircleApp {
 
     public static void main(String[] args) {
         Input userInput = new Input();
-        System.out.println("What is the radius of a circle?");
-        double radius = userInput.getDouble();
-        Circle circle = new Circle(radius);
-        System.out.printf("Area of the circle is: %f\n", circle.getArea());
-        System.out.printf("Circumference of the circle is: %f\n", circle.getCircumference());
+        boolean userAnswer = true;
+        do {
+            double radius = userInput.getDouble(1,100);
+            Circle circle = new Circle(radius);
+
+            System.out.printf("Area of the circle is: %f\n", circle.getArea());
+            System.out.printf("Circumference of the circle is: %f\n", circle.getCircumference());
+
+            userAnswer = userInput.yesNo("Want to make check another circle? [Yes/No]");
+        } while (userAnswer);
+
+        System.out.println("You checked " + Circle.getCircleCount() + " circles.");
+
+
+//        System.out.println("What is the radius of a circle?");
+//        double radius = userInput.getDouble();
+//        Circle circle = new Circle(radius);
+//        System.out.printf("Area of the circle is: %f\n", circle.getArea());
+//        System.out.printf("Circumference of the circle is: %f\n", circle.getCircumference());
     }
 
 }
