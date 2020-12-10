1771c41717699927fa8fd2deaddf63af85f1c1e4
Evan Mead <evan.c.mead@gmail.com>
2020-12-10 09:39:07 -0600

    finished DishTools

--- src/DishTest.java
+++ src/DishTest.java
@@ -1,12 +1,26 @@
 public class DishTest {
 
     public static void main(String[] args) {
+
         Dish dish1 = new Dish();
         dish1.costInCents = 1200;
         dish1.nameOfDish = "Tasty Burger";
         dish1.wouldRecommend = true;
 
+        Dish dish2 = new Dish();
+        dish2.costInCents = 1900;
+        dish2.nameOfDish = "Spendy Burger";
+        dish2.wouldRecommend = false;
+
         dish1.printSummary();
+        dish2.printSummary();
+
+        DishTools.shoutDishName(dish1);
+        DishTools.analyzeDishCost(dish1);
+        DishTools.flipRecommendation(dish1);
+        DishTools.shoutDishName(dish2);
+        DishTools.analyzeDishCost(dish2);
+        DishTools.flipRecommendation(dish2);
     }
 
 }
--- src/DishTools.java
+++ src/DishTools.java
@@ -2,12 +2,20 @@ public class DishTools {
 
     public static final int AVERAGE_COST_OF_DISH_IN_CENTS = 1300;
 
-    public void shoutDishName(String name) {
-        System.out.printf("%s", name);
+    public static void shoutDishName(Dish name) {
+        System.out.println(name.nameOfDish.toUpperCase());
     }
 
-    public static void analyzeDishCost() {
+    public static void analyzeDishCost(Dish cost) {
+        if (cost.costInCents > AVERAGE_COST_OF_DISH_IN_CENTS) {
+            System.out.println("More expensive than average.");
+        } else {
+            System.out.println("Less expensive than average.");
+        }
+    }
 
+    public static void flipRecommendation(Dish recommend) {
+        System.out.println(!recommend.wouldRecommend);
     }
 
 }
