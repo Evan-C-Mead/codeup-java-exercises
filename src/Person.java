6d7b1821565513d5c3f6b92e971bbd32463f898a
Evan Mead <evan.c.mead@gmail.com>
2020-12-10 10:53:09 -0600

    moved main method

--- src/Person.java
+++ src/Person.java
@@ -1,31 +1,5 @@
 public class Person {
 
-    public static void main(String[] args) {
-        Person person1 = new Person("Dude");
-        System.out.println(person1.name);
-        person1.sayHello();
-
-//        Person person2 = new Person("John");
-//        Person person3 = new Person("John");
-//        System.out.println(person1.getName().equals(person2.getName()));
-//        System.out.println(person2 == person3);
-
-//        Person person4 = new Person("Mark");
-//        Person person5 = person4;
-//        System.out.println(person4 == person5);
-
-//        Person person6 = new Person("John");
-//        Person person7 = person6;
-//        System.out.println(person6.getName());
-//        System.out.println(person7.getName());
-//        person7.setName("Jane");
-//        System.out.println(person6.getName());
-//        System.out.println(person6);
-//        System.out.println(person7.getName());
-//        System.out.println(person7);
-
-    }
-
     private String name;
 
     public Person(String personName) {
@@ -47,5 +21,29 @@ public class Person {
         System.out.printf("Hello, %s\n", name);
     }
 
+    public static void main(String[] args) {
+        Person person1 = new Person("Dude");
+        System.out.println(person1.name);
+        person1.sayHello();
+
+//        Person person2 = new Person("John");
+//        Person person3 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person2 == person3);
+//
+//        Person person4 = new Person("Mark");
+//        Person person5 = person4;
+//        System.out.println(person4 == person5);
+//
+//        Person person6 = new Person("John");
+//        Person person7 = person6;
+//        System.out.println(person6.getName());
+//        System.out.println(person7.getName());
+//        person7.setName("Jane");
+//        System.out.println(person6.getName());
+//        System.out.println(person6);
+//        System.out.println(person7.getName());
+//        System.out.println(person7);
+    }
 
 }
