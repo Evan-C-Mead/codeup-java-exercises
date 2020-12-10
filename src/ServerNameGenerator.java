d99840ec86fda29e9c2299f06d661b98aa6e6678
Evan Mead <evan.c.mead@gmail.com>
2020-12-10 17:31:10 -0600

    server name generator created - working on randomizer method

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,11 @@
+public class ServerNameGenerator {
+
+    public static void main(String[] args) {
+
+    }
+
+    String[] adjectives = {"good", "full", "late", "dank", "strong", "important", "old", "special", "free", "clear"};
+    String[] nouns = {"school", "person", "finger", "magazine", "yard", "night", "child", "arena", "bat", "program"};
+
+
+}
