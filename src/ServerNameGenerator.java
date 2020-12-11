8293f9f6eb0b9854d60bbb8c0d55c04e586ab53a
Evan Mead <evan.c.mead@gmail.com>
2020-12-11 11:44:42 -0600

    randomizer complete - server name generator complete

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,11 +1,20 @@
+import java.util.Random;
+
 public class ServerNameGenerator {
 
     public static void main(String[] args) {
 
+        String[] adjectives = {"good", "full", "late", "dank", "strong", "important", "old", "special", "free", "clear"};
+        String[] nouns = {"school", "person", "finger", "magazine", "yard", "night", "child", "arena", "bat", "program"};
+
+        System.out.printf("Your server name is: %s %s", randomizer(adjectives), randomizer(nouns));
     }
 
-    String[] adjectives = {"good", "full", "late", "dank", "strong", "important", "old", "special", "free", "clear"};
-    String[] nouns = {"school", "person", "finger", "magazine", "yard", "night", "child", "arena", "bat", "program"};
 
 
+    public static String randomizer(String[] array) {
+        Random random = new Random();
+        int randomIndex = random.nextInt(array.length);
+        return array[randomIndex];
+    }
 }
