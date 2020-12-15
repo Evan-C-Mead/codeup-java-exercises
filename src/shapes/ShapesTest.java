4c729886a552086da7c8d6aa57a3a86e861ec9fa
Evan Mead <evan.c.mead@gmail.com>
2020-12-15 14:37:32 -0600

    abstract classes and interfaces exercise complete

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -4,13 +4,21 @@ public class ShapesTest {
 
     public static void main(String[] args) {
 
-        Rectangle box1 = new Rectangle(4, 5);
-        System.out.printf("Area of your rectangle is: %.1f\n", box1.getArea());
-        System.out.printf("Perimeter of your rectangle is: %.1f\n", box1.getPerimeter());
+        Measurable myShape = new Square(6);
+        System.out.printf("Area of your square is: %.1f\n", myShape.getArea());
+        System.out.printf("Perimeter of your square is: %.1f\n", myShape.getPerimeter());
 
-        Rectangle box2 = new Square(5);
-        System.out.printf("Area of your square is: %.1f\n", box2.getArea());
-        System.out.printf("Perimeter of your square is: %.1f\n", box2.getPerimeter());
+        Measurable myShape2 = new Rectangle(3,7);
+        System.out.printf("Area of your rectangle is: %.1f\n", myShape2.getArea());
+        System.out.printf("Perimeter of your rectangle is: %.1f\n", myShape2.getPerimeter());
+
+//        Rectangle box1 = new Rectangle(4, 5);
+//        System.out.printf("Area of your rectangle is: %.1f\n", box1.getArea());
+//        System.out.printf("Perimeter of your rectangle is: %.1f\n", box1.getPerimeter());
+
+//        Rectangle box2 = new Square(5);
+//        System.out.printf("Area of your square is: %.1f\n", box2.getArea());
+//        System.out.printf("Perimeter of your square is: %.1f\n", box2.getPerimeter());
     }
 
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,20 +1,35 @@
 package shapes;
 
-public class Square extends Rectangle{
+public class Square extends Quadrilateral{
 
     public Square(double side) {
         super(side, side);
     }
 
     @Override
-    public double getArea() {
-        System.out.println("Square implementation of area.");
-        return (Math.pow(length, 2));
+    public double setLength() {
+        return length;
     }
 
     @Override
-    public double getPerimeter() {
-        System.out.println("Square implementation of perimeter.");
-        return 4 * width;
+    public double setWidth() {
+        return width;
     }
+
+//    public Square(double side) {
+//        super(side, side);
+//    }
+//
+//    @Override
+//    public double getArea() {
+//        System.out.println("Square implementation of area.");
+//        return (Math.pow(length, 2));
+//    }
+//
+//    @Override
+//    public double getPerimeter() {
+//        System.out.println("Square implementation of perimeter.");
+//        return 4 * width;
+//    }
+
 }
