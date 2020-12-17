41fe8ec17f1741f0f235bb9bb32e4167b4e1cc38
Evan Mead <evan.c.mead@gmail.com>
2020-12-17 12:44:37 -0600

    reorganized grades application and created grocery list application

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -11,29 +11,28 @@ public class GradesApplication {
         HashMap<String, Student> students = new HashMap<>();
 
         Student Student1 = new Student("Jake", 100);
+        students.put("jakeTheSnake75", Student1);
         Student1.addGrade(76);
         Student1.addGrade(87);
         Student1.addGrade(93);
 
         Student Student2 = new Student("Mary", 100);
+        students.put("mary-mary-quite-contrary", Student2);
         Student2.addGrade(89);
         Student2.addGrade(75);
         Student2.addGrade(82);
 
         Student Student3 = new Student("Frank", 100);
+        students.put("frankThe-tank-90", Student3);
         Student3.addGrade(97);
         Student3.addGrade(83);
         Student3.addGrade(70);
 
         Student Student4 = new Student("Rodrigo", 100);
+        students.put("rodrigoIsADude02", Student4);
         Student4.addGrade(81);
         Student4.addGrade(93);
         Student4.addGrade(78);
-
-        students.put("jakeTheSnake75", Student1);
-        students.put("mary-mary-quite-contrary", Student2);
-        students.put("frankThe-tank-90", Student3);
-        students.put("rodrigoIsADude02", Student4);
 
         commandListInterface(students);
     }
---
+++ src/grades/GroceryListApplication.java
@@ -0,0 +1,67 @@
+package grades;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import util.Input;
+
+public class GroceryListApplication {
+
+//    ArrayList<String> meat = new ArrayList<>(Arrays.asList("Beef", "Chicken", "Pork"));
+//    ArrayList<String> fruit = new ArrayList<>(Arrays.asList("Apple", "Pear", "Orange", "Banana"));
+//    ArrayList<String> vegetables = new ArrayList<>(Arrays.asList("Carrot", "Radish", "Potato", "Cabbage"));
+//    ArrayList<String> dairy = new ArrayList<>(Arrays.asList("Milk", "Butter", "Yogurt"));
+//    ArrayList<String> dryGoods = new ArrayList<>(Arrays.asList("Cereal", "Coffee", "Sugar"));
+
+    HashMap<ArrayList<String>, String> foods = new HashMap<>();
+
+    public static void main(String[] args) {
+        Input input = new Input();
+        boolean userContinue = true;
+
+        System.out.println("Would you like to create a grocery list?");
+        userContinue = input.yesNo();
+        if (userContinue) {
+            System.out.println("Would you like to add a new item?");
+            boolean proceedToCategory = input.yesNo();
+            if (proceedToCategory) {
+                chooseGroceryCategory();
+            }
+        }
+    }
+
+    public static void chooseGroceryCategory() {
+        ArrayList<String> foodCategories = new ArrayList<>(Arrays.asList("Meat", "Fruit", "Vegetables", "Dairy", "Dry Goods"));
+        int i = 1;
+
+        System.out.println("What category would you like to browse?");
+        for (String category : foodCategories) {
+            System.out.println(i + " - " + category);
+            i++;
+        }
+//        switch (foodCategories) {
+//            case 1:
+////                code
+//                break;
+//            case 2:
+////                code
+//                break;
+//            case 3:
+////                code
+//                break;
+//        }
+    }
+
+}
+
+// 1) Given an orderes list of groceries to choose from, select the category
+//                        display prewritten lidt of categories
+//                            What category?
+//                                save user input for category, item, and quantity to insert to HashMap
+
+//                2) Enter the name of the item
+//                      what is the item?
+//
+//                3) Enter how many of the item
+//                      what is the quantity?
+//                          would yo like to add another item? - if no exit, if yes repeat step 1 - 3
